21466a77822081237de5e7940171731349fab2b5
Jungmin Ji <jungmin.ji@dreams.io>
2025-12-26 12:38:46 +0900

    fix: add vercel.live to CSP frame-src

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app https://www.google.com www.googletagmanager.com https://www.youtube.com https://www.youtube-nocookie.com;
+  frame-src giscus.app https://www.google.com www.googletagmanager.com https://www.youtube.com https://www.youtube-nocookie.com https://vercel.live;
 `
 
 const securityHeaders = [
