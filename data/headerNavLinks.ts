6f5e783d180097de7f5ea64bd821c8749259167a
Jungmin Ji <jungmin.ji@dreams.io>
2025-12-26 12:35:35 +0900

    feat: add Tools link to header navigation

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,6 +4,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/tools', title: 'Tools' },
 ]
 
 export default headerNavLinks
