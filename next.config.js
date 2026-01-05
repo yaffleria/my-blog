52e0b44f0420fe8e884e22d02176e3b60a1c4a2b
yaffleria <yaffleria@gmail.com>
2026-01-05 20:01:07 +0900

    chore: allow vercel.live font in CSP for Chrome extension

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const ContentSecurityPolicy = `
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
-  font-src 'self';
+  font-src 'self' https://vercel.live;
   frame-src giscus.app https://www.google.com www.googletagmanager.com https://www.youtube.com https://www.youtube-nocookie.com https://vercel.live;
 `
 
